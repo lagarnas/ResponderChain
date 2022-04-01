9c1ffc46d1bcb6c2ecceeb4e718ada5f7fa5bf17
lagarnas <lagarnas@gmail.com>
2022-04-01 10:26:27 +0400

    first commit

--- ResponderChain/ViewController.swift
+++ ResponderChain/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
+extension UIView {
+  open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    print(self, #function)
+    next?.touchesBegan(touches, with: event)
+  }
+}
+
+
+extension UIWindow {
+  open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    print("UIWindow:", #function)
+    next?.touchesBegan(touches, with: event)
+  }
+}
+
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
-
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    print("UIViewController:", #function)
+    next?.touchesBegan(touches, with: event)
+  }
 
 }
 
